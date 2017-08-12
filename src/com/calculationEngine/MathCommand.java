1132d047f10d625f29260e04257c1c5c5e129f0b
Neo Yiu <neo.yiu@rakuten.com>
2017-08-12 19:04:00 +0100

    added other source code

---
+++ src/com/calculationEngine/MathCommand.java
@@ -0,0 +1,11 @@
+package com.calculationEngine;
+
+/**
+ * Created by neo.yiu on 09/08/2017.
+ */
+public enum MathCommand {
+    Add,
+    Subtract,
+    Multiply,
+    Divide
+}
---
+++ src/com/calculationEngine/Multiplier.java
@@ -0,0 +1,17 @@
+package com.calculationEngine;
+
+/**
+ * Created by neo.yiu on 07/08/2017.
+ */
+public class Multiplier extends CalculateBase{
+    public Multiplier(){}
+    public Multiplier(double leftVal, double rightVal){
+        super(leftVal, rightVal);
+    }
+
+    @Override
+    public void calculate() {
+        double value = getLeftVal() * getRightVal();
+        setResult(value);
+    }
+}
